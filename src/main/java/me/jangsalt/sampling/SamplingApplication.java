ce90c0c341c58dd75bcb4619c2dfa2b9e836a20e
jangsalt <jangsalt@gmail.com>
2023-02-23 15:04:28 +0900

    Init Project

---
+++ src/main/java/me/jangsalt/sampling/SamplingApplication.java
@@ -0,0 +1,13 @@
+package me.jangsalt.sampling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/jangsalt/sampling/SamplingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jangsalt.sampling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
